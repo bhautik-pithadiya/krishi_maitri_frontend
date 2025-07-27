0cb4f276c440f7db5f14c3484067f23517d9538b
Meet Makwana <meet.makwana@weboccult.com>
2025-07-27 08:59:10 +0530

    UI bug fixes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,12 +4,7 @@
       <div class="flex justify-between items-center">
         <!-- Logo -->
         <div class="flex items-center space-x-2">
-          <div class="w-10 h-10 bg-primary rounded-full flex items-center justify-center">
-            <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
-            </svg>
-          </div>
+          <img src="@/assets/images/logo.png" alt="Krishi Maitri Logo" class="w-10 h-10">
           <router-link to="/" class="text-xl font-bold text-gray-800">
             Krishi Maitri
           </router-link>
